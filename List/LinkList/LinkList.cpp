e390737567f20b0cce32c00a4ec3f74db502229e
zhouxc <zhouxiaochun_000@163.com>
2018-11-30 17:29:56 +0800

    Add reverse() and print function in LinkList

--- List/LinkList/LinkList.cpp
+++ List/LinkList/LinkList.cpp
@@ -1,24 +1,19 @@
-﻿// LinkList.cpp: 定义应用程序的入口点。
-//
-
 #include "LinkList.h"
 
 #include <stdexcept>
+#include <iostream>
 
 using namespace std;
 
 LinkList::LinkList()
-    : head_(Node(-1))
-{
+        : head_(Node(-1)) {
 }
 
-LinkList::~LinkList()
-{
+LinkList::~LinkList() {
     clear();
 }
 
-void LinkList::push_back(int data)
-{
+void LinkList::push_back(int data) {
     auto p = &head_;
     while (p->next) p = p->next;
 
@@ -26,26 +21,23 @@ void LinkList::push_back(int data)
     p->next = newNode;
 }
 
-void LinkList::push_front(int data)
-{
+void LinkList::push_front(int data) {
     auto newNode = new Node(data, head_.next);
 
     head_.next = newNode;
 }
 
-void LinkList::insert(int data, size_t pos)
-{
+void LinkList::insert(int data, size_t pos) {
     auto p = &head_;
     size_t i = 0;
-    while (p->next && i++ != pos) 
+    while (p->next && i++ != pos)
         p = p->next;
 
     auto newNode = new Node(data, p->next);
     p->next = newNode;
 }
 
-int LinkList::pop_back()
-{
+int LinkList::pop_back() {
     if (empty()) throw runtime_error("LinkList is empty.");
 
     auto p = head_.next;
@@ -60,8 +52,7 @@ int LinkList::pop_back()
     return ret;
 }
 
-int LinkList::pop_front()
-{
+int LinkList::pop_front() {
     if (empty()) throw runtime_error("LinkList is empty.");
 
     auto p = head_.next;
@@ -73,8 +64,7 @@ int LinkList::pop_front()
     return ret;
 }
 
-void LinkList::remove(int data)
-{
+void LinkList::remove(int data) {
     auto p = &head_;
 
     while (p->next) {
@@ -88,8 +78,7 @@ void LinkList::remove(int data)
     }
 }
 
-void LinkList::remove_at(size_t pos)
-{
+void LinkList::remove_at(size_t pos) {
     auto p = &head_;
     size_t i = 0;
     while (p->next) {
@@ -104,8 +93,7 @@ void LinkList::remove_at(size_t pos)
     }
 }
 
-void LinkList::clear()
-{
+void LinkList::clear() {
     while (head_.next) {
         auto p = head_.next;
         head_.next = p->next;
@@ -113,8 +101,7 @@ void LinkList::clear()
     }
 }
 
-size_t LinkList::find(int data)
-{
+size_t LinkList::find(int data) {
     auto p = &head_;
     size_t i = 0;
     while (p->next) {
@@ -128,8 +115,7 @@ size_t LinkList::find(int data)
     return npos;
 }
 
-size_t LinkList::length() const
-{
+size_t LinkList::length() const {
     auto p = &head_;
     size_t i = 0;
     while (p->next)
@@ -138,18 +124,15 @@ size_t LinkList::length() const
     return i;
 }
 
-bool LinkList::empty() const
-{
+bool LinkList::empty() const {
     return head_.next == nullptr;
 }
 
-int& LinkList::operator[](size_t pos)
-{
+int &LinkList::operator[](size_t pos) {
     auto p = head_.next;
     size_t i = 0;
     while (p) {
         if (i++ == pos) break;
-
         p = p->next;
     }
 
@@ -158,3 +141,30 @@ int& LinkList::operator[](size_t pos)
     return p->data;
 }
 
+void LinkList::reverse() {
+    auto p = head_.next;
+    if (!p) return;
+
+    while (p->next) {
+        auto temp = p->next;
+        p->next = temp->next;
+        temp->next = head_.next;
+        head_.next = temp;
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const LinkList &list) {
+    os << "[";
+
+    auto p = list.head_.next;
+    while (p) {
+        os << p->data
+           << (p->next ? ", " : "");
+
+        p = p->next;
+    }
+
+    os << "]";
+
+    return os;
+}
--- List/LinkList/main.cpp
+++ List/LinkList/main.cpp
@@ -1,5 +1,9 @@
 #include "LinkList.h"
 
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char* argv[])
 {
     LinkList list;
@@ -9,6 +13,8 @@ int main(int argc, char* argv[])
     list.push_front(4);
 
     list.insert(999, 0);
+
+    cout << list << endl;
 
     int value = list.pop_back();
 
@@ -21,9 +27,24 @@ int main(int argc, char* argv[])
     size_t len = list.length();
 
     int val = -1;
-    val = list[100];
+    val = list[2];
     list[1] = 333;
     val = list[1];
+
+    list.push_back(3);
+    list.push_back(10);
+    list.push_front(-3);
+    cout << list << endl;
+
+    list.reverse();
+    cout << list << endl;
+
+    val = list.pop_front();
+    cout << list << "\tPop Value: " << val << endl;
+
+    list.clear();
+    list.reverse();
+    cout << list << endl;
 
     return 0;
 }
